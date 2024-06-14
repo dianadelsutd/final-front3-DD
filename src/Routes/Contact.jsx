6c6120b2d95c3fdc726b220622bdde6745f0be1e
dianadelsutd <dianadelgadosuarez@192.168.1.4>
2024-06-14 17:55:06 -0300

    primer commit de prueba

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Form from '../Components/Form'
+import React from 'react';
+import Form from '../Components/Form';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,10 +7,11 @@ const Contact = () => {
   return (
     <div>
       <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
-      <Form/>
-    </div>
-  )
-}
+      <p>Send us your questions and we will contact you!!</p>
 
-export default Contact
+      <Form />
+    </div>
+  );
+};
+
+export default Contact;
